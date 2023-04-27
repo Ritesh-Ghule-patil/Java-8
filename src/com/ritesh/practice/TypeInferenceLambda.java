d74b0fd67ee6b6b9a0db8e9c5895ff057b1d1ad5
Ritesh <riteshghule5050@gmail.com>
2023-04-27 22:49:40 +0530

    lamda

---
+++ src/com/ritesh/practice/TypeInferenceLambda.java
@@ -0,0 +1,18 @@
+package com.ritesh.practice;
+
+import com.ritesh.interfaces.Greeting;
+
+public class TypeInferenceLambda {
+    public static void main(String[] args) {
+
+        String name = "Ritesh";
+
+        Greeting birthday = str -> "Happy Birthday Dear " + str;
+        System.out.println(birthday.greet(name));
+
+        Greeting anniversary = str-> "Happy Anniversary Dear "+name;
+
+        System.out.println(anniversary.greet(name));
+    }
+
+}
