3f4fa33b60b085f84b04daa5e0537d7b25d5ca69
Ritesh <riteshghule5050@gmail.com>
2023-04-28 23:40:33 +0530

    Functional interface use of predicate which take one parameter and give return boolean result

---
+++ src/com/ritesh/practice/ExceptionHandelingByLambda.java
@@ -0,0 +1,27 @@
+package com.ritesh.practice;
+
+import java.util.function.BiConsumer;
+
+public class ExceptionHandelingByLambda {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5};
+
+        int key = 0;
+
+        process(arr,key,(k,v)-> {
+            try {
+                System.out.println(k / v);
+            }
+            catch (ArithmeticException e){
+                System.out.println("You can't Divide a number by Zero");
+            }
+        });
+    }
+
+    private static void process(int[] arr, int key, BiConsumer<Integer,Integer> consumer) {
+        for (int i:arr ) {
+            consumer.accept(i,key);
+        }
+    }
+}
