dcaf732ec26074ca001e59786564e77063175f71
Ritesh <riteshghule5050@gmail.com>
2023-05-02 09:03:40 +0530

    Functional Interface With static and default method

--- src/com/ritesh/interfaces/Greeting.java
+++ src/com/ritesh/interfaces/Greeting.java
@@ -1,5 +1,13 @@
 package com.ritesh.interfaces;
 @FunctionalInterface
 public interface Greeting {
-    public String greet(String name);
+     String greet(String name);
+
+     default void greetHello(){
+        System.out.println("Hello, How Are You..!");
+    }
+
+     static void geetBye(){
+        System.out.println("Nice To Interact With You, Bye Bye Will Meet Soon...!");
+    }
 }
